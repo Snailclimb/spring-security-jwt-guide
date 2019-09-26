04c89b7e77098328257e0c62f6c9859fb0a618ac
Kou Shuang <koushuangbwcx@163.com>
2019-09-26 22:47:28 +0800

    Delete init.sql

--- src/main/resources/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `password` varchar(255) DEFAULT NULL,
-  `role` varchar(255) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
