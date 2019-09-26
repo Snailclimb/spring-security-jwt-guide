89b004de5d2d4c5292e8a0e96518089d23e92b6b
Kou Shuang <koushuangbwcx@163.com>
2019-09-26 20:44:09 +0800

    feat:exposed headers to client

--- src/main/java/github/javaguide/springsecurityjwtguide/security/config/CrosConfiguration.java
+++ src/main/java/github/javaguide/springsecurityjwtguide/security/config/CrosConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 /**
  * @author shuang.kou
  * 跨域配置
@@ -15,6 +17,9 @@ public class CrosConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
+                //暴露header中的其他属性给客户端应用程序
+                //如果不设置这个属性前端无法通过response header获取到Authorization也就是token
+                .exposedHeaders("Authorization")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .maxAge(3600);
