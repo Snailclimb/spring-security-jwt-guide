7c8b3ee1c0a38428ea9cdbbda6c66e8a6914ffd4
BoyanChan <chankaser@gmail.com>
2019-10-09 16:10:10 +0800

    无rememberMe时过期时间的修复

--- src/main/java/github/javaguide/springsecurityjwtguide/security/constants/SecurityConstants.java
+++ src/main/java/github/javaguide/springsecurityjwtguide/security/constants/SecurityConstants.java
@@ -17,7 +17,7 @@ public class SecurityConstants {
     /**
      * rememberMe 为 false 的时候过期时间是1个小时
      */
-    public static final long EXPIRATION = 6L;
+    public static final long EXPIRATION = 60L * 60L;
     /**
      * rememberMe 为 true 的时候过期时间是7天
      */
