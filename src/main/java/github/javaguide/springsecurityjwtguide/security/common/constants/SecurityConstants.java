747f810d0db1a99af585091afe9ca01442d81a24
Saving <853029827@qq.com>
2020-11-28 14:45:18 +0800

    1.优化pom.xml文件 2.优化securityConfiguration和Cors配置优化

--- src/main/java/github/javaguide/springsecurityjwtguide/security/common/constants/SecurityConstants.java
+++ src/main/java/github/javaguide/springsecurityjwtguide/security/common/constants/SecurityConstants.java
@@ -10,10 +10,12 @@ public final class SecurityConstants {
      * 角色的key
      **/
     public static final String ROLE_CLAIMS = "rol";
+
     /**
      * rememberMe 为 false 的时候过期时间是1个小时
      */
     public static final long EXPIRATION = 60 * 60L;
+
     /**
      * rememberMe 为 true 的时候过期时间是7天
      */
@@ -25,11 +27,27 @@ public final class SecurityConstants {
     public static final String JWT_SECRET_KEY = "C*F-JaNdRgUkXn2r5u8x/A?D(G+KbPeShVmYq3s6v9y$B&E)H@McQfTjWnZr4u7w";
 
     // JWT token defaults
-
     public static final String TOKEN_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String TOKEN_TYPE = "JWT";
 
+    // Swagger WHITELIST
+    public static final String[] SWAGGER_WHITELIST = {
+            "/swagger-ui.html",
+            "/swagger-ui/*",
+            "/swagger-resources/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/webjars/**"
+    };
+
+    // 登录接口 WHITELIST
+    public static final String LOGIN_WHITELIST = "/api/auth/login";
+
+    // 过滤ALL
+    public static final String FILTER_ALL = "/api/**";
+
     private SecurityConstants() {
     }
+
 }
